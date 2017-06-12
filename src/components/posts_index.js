5b37b01c9d9c69ccd2e7b72cf4c2a066de62d921
man0ffire <iliqqnev88@gmail.com>
2017-06-12 18:16:42 +0300

    setting react router

---
+++ src/components/posts_index.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+export default class PostIndex extends Component {
+  render() {
+    return (
+      <div className='container'>
+        Posts Index
+      </div>
+    );
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,8 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import reducers from './reducers';
+import PostsIndex from './components/posts_index';
+
+import { BrowserRouter, Route } from 'react-router-dom';
+
+const createStoreWithMiddleware = applyMiddleware()(createStore);
 
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={createStoreWithMiddleware(reducers)}>
+    <BrowserRouter>
+      <div>
+        <Route path='/' component={PostsIndex} />
+      </div>
+    </BrowserRouter>
+  </Provider>
+  , document.getElementById('root'));
 registerServiceWorker();
