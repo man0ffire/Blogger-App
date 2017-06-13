f00d011a256751848198a7de1e5557d3fe3d0e2b
man0ffire <iliqqnev88@gmail.com>
2017-06-13 20:07:31 +0300

    added alot of stuff

---
+++ src/components/posts_new.js
@@ -0,0 +1,48 @@
+import React, {Component} from 'react';
+import {Field, reduxForm} from 'redux-form';
+
+class PostsNew extends Component {
+  renderField(field){
+    return (
+      <div className='form-group'>
+        <label>{field.label}</label>
+        <input
+          className='form-control'
+          type='text'
+          {...field.input}
+        />
+      </div>
+    );
+  }
+
+  renderTagsField(){
+
+  }
+
+
+  render() {
+    return (
+      <form>
+        <Field
+          label='Title'
+          name='title'
+          component={this.renderField}
+        />
+        <Field
+          label='Categories'
+          name='categories'
+          component={this.renderField}
+        />
+        <Field
+          label='Post Content'
+          name='content'
+          component={this.renderField}
+        />
+      </form>
+    );
+  }
+}
+
+export default reduxForm({
+  form: 'PostsNewForm'
+})(PostsNew);
--- src/index.js
+++ src/index.js
@@ -5,17 +5,22 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import reducers from './reducers';
 import PostsIndex from './components/posts_index';
+import PostsNew from './components/posts_new';
+import promise from 'redux-promise';
 
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
 
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
     <BrowserRouter>
       <div>
-        <Route path='/' component={PostsIndex} />
+        <Switch>
+          <Route path='/posts/new' component={PostsNew} />
+          <Route path='/' component={PostsIndex} />
+        </Switch>
       </div>
     </BrowserRouter>
   </Provider>
